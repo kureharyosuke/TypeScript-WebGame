bffdb59d8c3940f4e9a703742d1539e6df531445
kureharyosuke <ohhayo.com@gmail.com>
2021-02-28 23:56:36 +0900

    add: gugudan

--- lecture/lecture.js
+++ lecture/lecture.js
@@ -6,7 +6,7 @@ var result = numOne * numTwo;
 // const word: HTMLDivElement
 // word의 타입은 HTMLDivElement
 var word = document.createElement('div');
-word.textContent = numOne + " * " + numTwo;
+word.textContent = numOne + " X " + numTwo + " =";
 document.body.append(word);
 //const word: HTMLDivElement
 // const form = document.createElement('form');
@@ -14,10 +14,32 @@ document.body.append(word);
 // form의 타입은 HTMLFormElement
 var form = document.createElement('form');
 //(method) Document.createElement<"div">(tagName: "div", options?: ElementCreationOptions): HTMLDivElement (+2 overloads)
-document.body.append('form');
+document.body.append(form);
 //(method) ParentNode.append(...nodes: (string | Node)[]): void
 // const input = document.createElement('input');
 // const input: HTMLInputElement
 var input = document.createElement('input');
 input.type = "number";
-form.append('input');
+form.append(input);
+var button = document.createElement('button');
+button.textContent = 'Button';
+//const button: HTMLButtonElement
+form.append(button);
+var resultDiv = document.createElement('div');
+document.body.append(resultDiv);
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (result === Number(input.value)) { // input.value = string => Number(input.value) or +(input.value)
+        resultDiv.textContent = 'Good Answer';
+        numOne = Math.ceil(Math.random() * 9);
+        numTwo = Math.ceil(Math.random() * 9);
+        result = numOne * numTwo;
+        word.textContent = numOne + " X " + numTwo + " =";
+        input.focus();
+    }
+    else {
+        resultDiv.textContent = 'No Answer';
+        input.value = '';
+        input.focus();
+    }
+});
--- lecture/lecture.ts
+++ lecture/lecture.ts
@@ -8,7 +8,7 @@ let result: number = numOne * numTwo;
 // const word: HTMLDivElement
 // word의 타입은 HTMLDivElement
 const word: HTMLDivElement = document.createElement('div');
-word.textContent = `${numOne} * ${numTwo}`;
+word.textContent = `${numOne} X ${numTwo} =`;
 document.body.append(word);
 //const word: HTMLDivElement
 
@@ -17,11 +17,35 @@ document.body.append(word);
 // form의 타입은 HTMLFormElement
 const form: HTMLFormElement = document.createElement('form');
 //(method) Document.createElement<"div">(tagName: "div", options?: ElementCreationOptions): HTMLDivElement (+2 overloads)
-document.body.append('form');
+document.body.append(form);
 //(method) ParentNode.append(...nodes: (string | Node)[]): void
 
 // const input = document.createElement('input');
 // const input: HTMLInputElement
 const input: HTMLInputElement = document.createElement('input');
 input.type = "number";
-form.append('input')
+form.append(input)
+
+const button: HTMLButtonElement = document.createElement('button');
+button.textContent = 'Button'
+//const button: HTMLButtonElement
+form.append(button);
+
+const resultDiv = document.createElement('div');
+document.body.append(resultDiv);
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (result === Number(input.value)) { // input.value = string => Number(input.value) or +(input.value)
+        resultDiv.textContent = 'Good Answer';
+        numOne = Math.ceil(Math.random() * 9);
+        numTwo = Math.ceil(Math.random() * 9);
+        result = numOne * numTwo;    
+        word.textContent = `${numOne} X ${numTwo} =`;
+        input.focus(); 
+    } else {
+        resultDiv.textContent = 'No Answer';
+        input.value = '';
+        input.focus();
+    }
+ })
